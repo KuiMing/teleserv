fa93b830a9a01621180359cf21ffe8d6d18a50b8
KuiMing <benjamin0901@gmail.com>
2022-02-23 21:02:32 +0800

    wehook and reply

--- application.py
+++ application.py
@@ -1,12 +1,37 @@
+from flask import Flask, request
+import telegram
+from telegram.ext import Dispatcher, MessageHandler, Filters
+import logging
 import os
-import json
-import requests
-from flask import Flask, request, abort
 
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+LOGGER = logging.getLogger(__name__)
+TOKEN = os.getenv("ACCESS_TOKEN")
+
+BOT = telegram.Bot(token=TOKEN)
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
     "hello world"
     return "Hello World!!!!!"
+
+@app.route("/callback", methods=['POST'])
+def callback() -> str:
+    if request.method == "POST":
+        update = telegram.Update.de_json(request.get_json(force=True), BOT)
+        dispatcher.process_update(update)
+    return 'ok'
+
+
+def reply_handler(update) -> None:
+    text = update.message.text
+    update.message.reply_text(text)
+
+dispatcher = Dispatcher(BOT, None)
+dispatcher.add_handler(MessageHandler(Filters.text, reply_handler))
