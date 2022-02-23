25a3f721f0ce17d1e6c2434b022df717df1cfde0
KuiMing <benjamin0901@gmail.com>
2022-02-23 15:52:35 +0800

    hello world with flask

---
+++ application.py
@@ -0,0 +1,12 @@
+import os
+import json
+import requests
+from flask import Flask, request, abort
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    "hello world"
+    return "Hello World!!!!!"
