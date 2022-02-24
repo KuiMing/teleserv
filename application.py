e5bac238c2c5649318434f452ab2cff418a63bb1
KuiMing <benjamin0901@gmail.com>
2022-02-24 20:46:24 +0800

    button

--- application.py
+++ application.py
@@ -1,13 +1,14 @@
-from flask import Flask, request
-import telegram
-from telegram.ext import Dispatcher, MessageHandler, Filters
 import logging
 import os
-
+from flask import Flask, request
+import telegram
+from telegram.ext import Dispatcher, MessageHandler, Filters, CommandHandler
+from telegram import InlineKeyboardMarkup, InlineKeyboardButton
 
 # Enable logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO)
 LOGGER = logging.getLogger(__name__)
 TOKEN = os.getenv("ACCESS_TOKEN")
 
@@ -21,6 +22,7 @@ def hello():
     "hello world"
     return "Hello World!!!!!"
 
+
 @app.route("/callback", methods=['POST'])
 def callback() -> str:
     if request.method == "POST":
@@ -33,5 +35,20 @@ def reply_handler(bot, update) -> None:
     text = update.message.text
     update.message.reply_text(text)
 
+
+def start(bot, update) -> None:
+    update.message.reply_text(
+        "Reference",
+        reply_markup=InlineKeyboardMarkup([[
+            InlineKeyboardButton(
+                'My Github', url='https://github.com/KuiMing/triathlon_azure'),
+            InlineKeyboardButton(
+                'Slides',
+                url='https://kuiming.github.io/Azureml_talk/output/index.html#/'
+            )
+        ]]))
+
+
 dispatcher = Dispatcher(BOT, None)
 dispatcher.add_handler(MessageHandler(Filters.text, reply_handler))
+dispatcher.add_handler(CommandHandler('start', start))
